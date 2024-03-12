c29f46c499d24632d26ba2bec627319d032182fc
Tezada <sean.calzada@kooapps.com>
2024-03-12 14:13:13 +0800

    REFACTORE: Refactor controller files

--- src/controllers/MainSceneController.cs
+++ src/controllers/MainSceneController.cs
@@ -11,37 +11,48 @@ public partial class MainSceneController : Node {
 
 	// Input Event Listener
     public override void _Input(InputEvent @event){
-		OnTappedGroundTile(@event);
+		if(@event is InputEventMouseButton){
+			OnTappedGroundTile(@event);
+		}
+		
     }
 
 	private void OnTappedGroundTile(InputEvent @event){
-		if(
-			@event is InputEventMouseButton mouseEvent &&
-			mouseEvent.ButtonIndex == MouseButton.Left &&
-			@event.IsPressed()
-		){
-			Vector2 mousePosition = mouseEvent.Position;
-			Vector2I tilePostion = mTileMap.LocalToMap(mousePosition);
-			TileData groundTileData = mTileMap.GetCellTileData(mTileMap.GROUND_LAYER, tilePostion);
-			Vector2I tileToAdd = new Vector2I(1,0); // Location of the white token in the tile asset
-		
-			if(groundTileData != null){
-				bool isPlaceable = (bool)groundTileData.GetCustomData(mTileMap.IS_PLACEABLE_CUSTOM_DATA);
-				if(isPlaceable){
-					TileData tokenPlacementTilemap = mTileMap.GetCellTileData(
-						mTileMap.TOKEN_PLACEMENT_LAYER, 
-						tilePostion
-					);
-					if(tokenPlacementTilemap == null){
-						mTileMap.SetCell(
-							mTileMap.TOKEN_PLACEMENT_LAYER, 
-							tilePostion, 
-							0, 
-							tileToAdd
-						);
-					} 
-				}	
-			}
+		InputEventMouseButton mouseEvent = (InputEventMouseButton)@event;
+
+		if(!(mouseEvent.ButtonIndex == MouseButton.Left && @event.IsPressed())){
+			return;
 		}
+            
+		Vector2 mousePosition = mouseEvent.Position;
+		Vector2I tilePostion = mTileMap.LocalToMap(mousePosition);
+		TileData groundTileData = mTileMap.GetCellTileData(mTileMap.GROUND_LAYER, tilePostion);
+		Vector2I tileToAdd = new Vector2I(1,0); // Location of the white token in the tile asset
+	
+		if(groundTileData == null){
+			return;
+		} 
+
+		bool isPlaceable = (bool)groundTileData.GetCustomData(mTileMap.IS_PLACEABLE_CUSTOM_DATA);
+
+		if(!isPlaceable){
+			return;
+		}
+			
+		TileData tokenPlacementTilemap = mTileMap.GetCellTileData(
+			mTileMap.TOKEN_PLACEMENT_LAYER, 
+			tilePostion
+		);
+
+		if(tokenPlacementTilemap != null){
+			return;
+		}
+					
+		mTileMap.SetCell(
+			mTileMap.TOKEN_PLACEMENT_LAYER, 
+			tilePostion, 
+			0, 			
+			tileToAdd
+		);		
 	}
 }
